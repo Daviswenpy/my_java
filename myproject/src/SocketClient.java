6519c85c0919218650151ce2370a4a9f9b6ad5fa
Daviswenpy <davis_wen@foxmail.com>
2020-09-04 08:17:28 +0800

    java 4

--- myproject/src/SocketClient.java
+++ myproject/src/SocketClient.java
@@ -7,7 +7,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+// 客户端
 public class SocketClient {
     public static void main(String[] args) {
         try {
